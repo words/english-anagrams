a1e5195986ac773f0127b3e6df09208175a35d17
Titus Wormer <tituswormer@gmail.com>
2020-02-14 10:53:04 +0100

    Add stats to `readme.md`

--- readme.md
+++ readme.md
@@ -38,6 +38,46 @@ Yields:
 Given a word, I’ll give you a list of anagrams, or an empty array if no anagrams
 were found.
 
+<!--stats start-->
+
+## Stats
+
+*   Words in dictionary:
+    **275K**
+*   Anagrams:
+    **53K**
+*   Words w/ anagrams:
+    **19%**
+*   Group of most anagrams:
+    **13**
+*   Longest group of anagrams:
+    **microphotographies**, **photomicrographies**
+
+## Top 20 matches
+
+*   **13**: apers, apres, asper, pares, parse, pears, prase, presa, rapes, reaps, spaer, spare, spear
+*   **13**: arets, aster, earst, rates, reast, resat, stare, stear, strae, tares, taser, tears, teras
+*   **12**: alerts, alters, artels, estral, laster, ratels, salter, slater, staler, stelar, talers, tarsel
+*   **12**: least, leats, salet, setal, slate, stale, steal, stela, taels, tales, teals, tesla
+*   **11**: anestri, antsier, nastier, ratines, resiant, retains, retinas, retsina, stainer, starnie, stearin
+*   **11**: angriest, angstier, astringe, ganister, gantries, granites, ingrates, rangiest, reasting, stearing, tasering
+*   **11**: easting, eatings, gainest, genista, ingates, ingesta, seating, tagines, tangies, teasing, tsigane
+*   **11**: palest, palets, pastel, peltas, petals, plaste, plates, pleats, septal, staple, tepals
+*   **10**: angries, earings, erasing, gainers, graines, reagins, regains, reginas, searing, seringa
+*   **10**: deros, doers, dores, dorse, doser, redos, resod, rodes, rosed, sored
+*   **10**: lapse, leaps, pales, peals, pelas, pleas, salep, sepal, spale, speal
+*   **9**: acers, acres, cares, carse, escar, races, scare, scrae, serac
+*   **9**: airts, artis, astir, raits, sitar, stair, stria, tarsi, tiars
+*   **9**: aretes, easter, eaters, reates, reseat, saeter, seater, steare, teaser
+*   **9**: aridest, asterid, astride, diaster, disrate, staider, staired, tardies, tirades
+*   **9**: arles, earls, laers, lares, laser, lears, rales, reals, seral
+*   **9**: aspers, parses, passer, prases, repass, spaers, spares, sparse, spears
+*   **9**: ates, east, eats, etas, sate, seat, seta, taes, teas
+*   **9**: capers, crapes, escarp, pacers, parsec, recaps, scrape, secpar, spacer
+*   **9**: carets, cartes, caster, caters, crates, cresta, reacts, recast, traces
+
+<!--stats end-->
+
 ## License
 
 [MIT][license] © [Zeke Sikelianos][author]
---
+++ stats.js
@@ -0,0 +1,104 @@
+'use strict'
+
+var u = require('unist-builder')
+var zone = require('mdast-zone')
+var words = require('an-array-of-english-words')
+var anagrams = require('./anagrams.json')
+
+module.exports = stats
+
+var keys = Object.keys(anagrams)
+var wordCount = words.length
+var anagramCount = keys.reduce((acc, d) => acc + anagrams[d].length, 0)
+var wordsWithAnagrams = anagramCount / words.length
+
+var longest = keys.reduce((acc, d) => (d.length > acc.length ? d : acc), '')
+
+var most = keys.reduce(
+  (acc, d) => (anagrams[d].length > (anagrams[acc] || []).length ? d : acc),
+  ''
+)
+
+function stats() {
+  return transform
+
+  function transform(tree) {
+    zone(tree, 'stats', onstats)
+  }
+
+  function onstats(start, _, end) {
+    var fragment = [
+      u('heading', {depth: 2}, [u('text', 'Stats')]),
+      u('list', [
+        u('listItem', [
+          u('paragraph', [
+            u('text', 'Words in dictionary:\n'),
+            u('strong', [
+              u(
+                'text',
+                wordCount.toLocaleString('en-US', {notation: 'compact'})
+              )
+            ])
+          ])
+        ]),
+        u('listItem', [
+          u('paragraph', [
+            u('text', 'Anagrams:\n'),
+            u('strong', [
+              u(
+                'text',
+                anagramCount.toLocaleString('en-US', {notation: 'compact'})
+              )
+            ])
+          ])
+        ]),
+        u('listItem', [
+          u('paragraph', [
+            u('text', 'Words w/ anagrams:\n'),
+            u('strong', [
+              u(
+                'text',
+                wordsWithAnagrams.toLocaleString('en-US', {style: 'percent'})
+              )
+            ])
+          ])
+        ]),
+        u('listItem', [
+          u('paragraph', [
+            u('text', 'Group of most anagrams:\n'),
+            u('strong', [u('text', String(anagrams[most].length))])
+          ])
+        ]),
+        u('listItem', [
+          u(
+            'paragraph',
+            [].concat(
+              u('text', 'Longest group of anagrams:\n'),
+              anagrams[longest].flatMap((d, i) => {
+                var node = u('strong', [u('text', d)])
+                return i ? [u('text', ', '), node] : [node]
+              })
+            )
+          )
+        ])
+      ]),
+      u('heading', {depth: 2}, [u('text', 'Top 20 matches')]),
+      u(
+        'list',
+        keys
+          .sort((a, b) => anagrams[b].length - anagrams[a].length)
+          .slice(0, 20)
+          .map(d =>
+            u('listItem', [
+              u('paragraph', [
+                u('strong', [u('text', anagrams[d].length)]),
+                u('text', ': ' + anagrams[d].join(', '))
+              ])
+            ])
+          )
+      )
+    ]
+
+    return [].concat(start || [], fragment, end || [])
+  }
+}
